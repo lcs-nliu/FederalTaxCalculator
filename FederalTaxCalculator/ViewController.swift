2614eb5b60dcee2b54fd899bb8cba99af1c2d620
Nicole Liu <nicoleliu_@outlook.com>
2019-11-01 20:27:28 -0400

    Created my outlets and actions

--- FederalTaxCalculator/ViewController.swift
+++ FederalTaxCalculator/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    // Connect name text field to controller with an outlet
+    @IBOutlet weak var nameTextField: UITextField!
+    // Connect income text field to controller with an outlet
+    @IBOutlet weak var incomeTextField: UITextField!
+    // Connected name and tax amount label to controller using an outlet
+    @IBOutlet weak var nameTaxDisplay: UILabel!
+    // Connected the tax rate label to controller using an outlet
+    @IBOutlet weak var taxRateDisplay: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+      }
+    
+    // MARK: Methods
+    // Created an action for when calculate tax owing button is pressed
+    @IBAction func calculateButtonPressed(_ sender: Any) {
     }
-
-
 }
 
